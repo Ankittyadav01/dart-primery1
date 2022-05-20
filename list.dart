f0902c9e2bcf0f82e263dd63e7c5028ac7862ce8
Ankit Yadav <ankityadav997036@gmail.com>
2022-05-20 11:58:35 +0530

    CREATING LIST ADDING A DATA IN LIST. REMOVING DATA FROM THE LIST. PRINTING LIST

---
+++ list.dart
@@ -0,0 +1,13 @@
+void main() {
+  //CREATING A LIST.
+  //  <String>  IT IS THE WAY OF DICLARING DATA TYPE OF A LIST.
+  List<String> name = ['Ankit', 'Vivek', 'Shikha', 'Supriya', 'Ravi'];
+
+  //ADDING A DATA IN LIST.
+  name.add('Santoshi');
+  //REMOVING DATA FROM THE LIST.
+  name.remove('Ravi');
+
+  //PRINTING LIST.
+  print(name);
+}
