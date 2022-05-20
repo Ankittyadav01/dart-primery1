92e940495f3fd17a557faa044f0fd4d31b2e6489
Ankit Yadav <ankityadav997036@gmail.com>
2022-05-20 11:15:26 +0530

    different type of variables in dart

---
+++ variables.dart
@@ -0,0 +1,21 @@
+void main() {
+  //print statement in dart.
+  print("hello world");
+
+  //String in dart.
+  String name = "ankit";
+  print(name);
+
+  // integer in dart.
+  int age = 30;
+  print(age);
+
+  //boolian in dart.
+  bool isNight = false;
+  print(isNight);
+
+  //dynamic data type can change in future.
+  dynamic name1 = "Ankit";
+  name1 = 30;
+  print(name1);
+}
